b9c546c7ec2fedae38470f26caad06458babc0a2
josedlg <josedelaguarda@outlook.com>
2019-03-28 09:19:20 -0400

    Initial Commit

---
+++ Tonsorial LIC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tonsorial LIC
+//
+//  Created by Jose Delaguarda on 3/28/19.
+//  Copyright © 2019 DLGTECH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
