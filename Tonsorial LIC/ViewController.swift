6e116c3a010395ba0c96258767c13be4552d9904
josedlg <josedelaguarda@outlook.com>
2019-05-28 23:18:53 -0400

    Tried to fix the google sign in error but couldn't get it to work

--- Tonsorial LIC/ViewController.swift
+++ Tonsorial LIC/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController{
     @IBOutlet weak var WebView: WKWebView!
     override func viewDidLoad(){
         super.viewDidLoad()
+    
+        let dictionaty = NSDictionary(object: "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36", forKey: "UserAgent" as NSCopying)
+        UserDefaults.standard.register(defaults: dictionaty as! [String : Any])
         
     let url = URL(string: "https://lictonsorial.resurva.com/book")
         let request = URLRequest(url: url!)
