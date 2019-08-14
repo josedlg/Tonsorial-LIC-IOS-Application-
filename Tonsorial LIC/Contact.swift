0c740b7965feb75ca325bd971fcb6144a5343a3e
josedlg <josedelaguarda@outlook.com>
2019-08-14 01:40:40 -0400

    included social media, and a few UI/UX changes

--- Tonsorial LIC/Contact.swift
+++ Tonsorial LIC/Contact.swift
@@ -12,18 +12,23 @@ import WebKit
 class Contact: UIViewController{
     @IBOutlet weak var webView: WKWebView!
     
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "https://www.google.com/maps/place/32-86+Steinway+St,+Astoria,+NY+11103/@40.7567727,-73.9212617,18z/data=!4m5!3m4!1s0x89c25f3b24799959:0xada63e6e78ea3666!8m2!3d40.7570534!4d-73.9208482")
+        let url = URL(string: "https://www.facebook.com/longislandcitybarber/")
         let request = URLRequest(url: url!)
         
         webView.load(request)
+        
+    
        
         
 }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+
 }
 
