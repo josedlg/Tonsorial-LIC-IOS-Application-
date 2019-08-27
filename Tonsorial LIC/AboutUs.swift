8dcc84db2ab2dc4ecbea20abbb4da449621d00ed
josedlg <josedelaguarda@outlook.com>
2019-08-27 14:26:15 -0400

    Added social media on Contact page

--- Tonsorial LIC/AboutUs.swift
+++ Tonsorial LIC/AboutUs.swift
@@ -9,31 +9,15 @@
 import UIKit
 
 class AboutUs : UIViewController {
-    @IBOutlet weak var mainScrollView: UIScrollView!
-    
-    var imageArray = [UIImage]()
-    
-    
-    
+   
     override func viewDidLoad(){
         super.viewDidLoad()
-        mainScrollView.frame = view.frame
-    imageArray = [#imageLiteral(resourceName: "About Us-1"), #imageLiteral(resourceName: "slide2"),#imageLiteral(resourceName: "slide5"),#imageLiteral(resourceName: "slide7"),#imageLiteral(resourceName: "slide4"), #imageLiteral(resourceName: "IMG_0266-1"),#imageLiteral(resourceName: "slide3")]
-        
-        for i in 0..<imageArray.count{
-            let imageView = UIImageView()
-            imageView.image = imageArray[i]
-            imageView.contentMode = .scaleAspectFit
-            let xPosition = self.view.frame.width * CGFloat(i)
-            imageView.frame = CGRect(x: xPosition, y: 0, width: self.mainScrollView.frame.width, height: self.mainScrollView.frame.height)
-        
-            mainScrollView.contentSize.width = mainScrollView.frame.width * CGFloat(i + 1)
-            mainScrollView.addSubview(imageView)
+       
         }
-    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-}
 
+
+}
--- Tonsorial LIC/Contact.swift
+++ Tonsorial LIC/Contact.swift
@@ -16,19 +16,47 @@ class Contact: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "https://www.facebook.com/longislandcitybarber/")
+        let url = URL(string: "https://goo.gl/maps/anbVAdkSFseouPj58")
         let request = URLRequest(url: url!)
         
         webView.load(request)
-        
     
-       
-        
+    
 }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
+    @IBAction func fbButtonClicked(_ sender: UIButton) {
+        let instagram = URL(string: "https://facebook.com/longislandcitybarber/")!
+        
+        if UIApplication.shared.canOpenURL(instagram) {
+            UIApplication.shared.open(instagram, options: [UIApplication.OpenExternalURLOptionsKey(rawValue: ""):""], completionHandler: nil)
+        } else {
+            print("facebook not installed")
+        }
+    }
+    
+    @IBAction func igButtonClicked(_ sender: UIButton) {
+        let instagram = URL(string: "https://instagram.com/lic_tonsorial/")!
+        
+        if UIApplication.shared.canOpenURL(instagram) {
+            UIApplication.shared.open(instagram, options: [UIApplication.OpenExternalURLOptionsKey(rawValue: ""):""], completionHandler: nil)
+        } else {
+            print("Instagram not installed")
+        }
+    }
+    @IBAction func twButtonClicked(_ sender: UIButton) {
+        let instagram = URL(string: "https://twitter.com/Lic_tonsorial")!
+        
+        if UIApplication.shared.canOpenURL(instagram) {
+            UIApplication.shared.open(instagram, options: [UIApplication.OpenExternalURLOptionsKey(rawValue: ""):""], completionHandler: nil)
+        } else {
+            print("Twitter not installed")
+        }
+    }
 }
 
